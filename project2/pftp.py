86c44da83cab28fa495f5bad5a0a8a0ed8cbdddf
Ruolin Zheng <zheng_ruolin@hotmail.com>
2018-11-17 15:55:32 -0600

    Refactor handlers for multi-threading

--- project2/pftp.py
+++ project2/pftp.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import argparse, os, sys, socket, re
+import argparse, os, sys, socket, re, threading
 
 exit_codes = {
   0: 'Operation successfully completed',
@@ -13,40 +13,20 @@ exit_codes = {
   7: 'Generic error',
 }
 
-serv_resps = {
-  500: 4,
-  530: 2,
-  550: 3,
-}
-
 class Session(object):
-  def __init__(self, *args, **kwargs):
-    self.file = kwargs.get('file')
-    self.hostname = kwargs.get('hostname')
-    self.port = 21 if kwargs.get('port') is None else kwargs.get('port')
-    self.username = 'anonymous' if kwargs.get('username') is None \
-    else kwargs.get('username')
-    self.password = 'user@localhost.localnet' if kwargs.get('password') \
-    is None else kwargs.get('password')
+  def __init__(self, *args):
+    self.file = args[0] or None
+    self.hostname = args[1] or None
+    self.port = args[2] or 21
+    self.username = args[3] or 'anonymous'
+    self.password = args[4] or 'user@localhost.localnet'
 
 def myexit(errno):
-  print('Exit Code: ' + str(errno) + ' - ' + \
-    exit_codes.get(errno, exit_codes.get(7)))
+  print(f'Exit {str(errno)}: - {exit_codes.get(errno, exit_codes.get(7))}')
   sys.exit(errno)
 
-def check_code(response, expected):
-  if not response == expected:
-    myexit(serv_resps.get(response, 7))
-  return
-
-def print_log(fname, log_list):
-  if fname == '-':
-    for log in log_list:
-      print(log, end='')
-  else:
-    with open(fname, 'w') as fout:
-      for log in log_list:
-        fout.write(log)
+def check_code(result, expected):
+  pass
 
 def parse_args():
   parser = argparse.ArgumentParser()
@@ -54,6 +34,8 @@ def parse_args():
     application', action='store_true')
   parser.add_argument('-f', '--file', type=str, help='specifies the file to \
     download')
+  parser.add_argument('-t', '--thread', type=str, help='specifies the \
+    para-config-file for multi-thread downloads')
   parser.add_argument('-s', '--hostname', type=str, help='specifies the server \
     to download the file from')
   parser.add_argument('-p', '--port', type=int, help='specifies the port to be \
@@ -72,10 +54,22 @@ def parse_args():
 
   return args
 
-def handle_session(sess):
-  log_list = []
-  response = None
+def request_handler(sock, request, logfn):
+  sock.send(request.encode())
+  print(f'C->S: {request}', end='')
+  return None
 
+def response_handler(sock, exp_code, logfn):
+  response = sock.recv(255).decode()
+  print(f'S->C: {response}', end='')
+  check_code(int(response[:3]), exp_code)
+  if int(response[:3]) == 227:
+    data_port = re.findall(r'\d+', response)[-2:]
+    data_port = int(data_port[0]) * 256 + int(data_port[1])
+    return data_port
+  return None
+
+def session_handler(sess, logfn):
   # Control Process
   ctrl_sock = socket.socket()
   try:
@@ -83,43 +77,14 @@ def handle_session(sess):
   except:
     myexit(1)
 
-  # Connect
-  response = ctrl_sock.recv(255).decode()
-  serv_code = int(response[:3])
-  check_code(serv_code, 220)
-  log_list.append(f'S->C: {response[:3]} Server {sess.hostname}\n')
-
-  # USER
-  request = f'USER {sess.username}\r\n'
-  ctrl_sock.send(request.encode())
-  log_list.append(f'C->S: {request}')
-
-  response = ctrl_sock.recv(255).decode()
-  log_list.append(f'S->C: {response}')
-  user_code = int(response[:3])
-  check_code(user_code, 331)
-
-  # PASS
-  request = f'PASS {sess.password}\r\n'
-  ctrl_sock.send(request.encode())
-  log_list.append(f'C->S: {request}')
-
-  response = ctrl_sock.recv(255).decode()
-  log_list.append(f'S->C: {response}')
-  pass_code = int(response[:3])
-  check_code(pass_code, 230)
-
-  # PASV
-  request = f'PASV\r\n'
-  ctrl_sock.send(request.encode())
-  log_list.append(f'C->S: {request}')
-
-  response = ctrl_sock.recv(255).decode()
-  log_list.append(f'S->C: {response}')
-  pasv_code = int(response[:3])
-  check_code(pasv_code, 227)
-  data_port = re.findall(r'\d+', response)[-2:]
-  data_port = int(data_port[0]) * 256 + int(data_port[1])
+  # Connect -> USER -> PASS -> PASV
+  response_handler(ctrl_sock, 220, None)
+  request_handler(ctrl_sock, f'USER {sess.username}\r\n', None)
+  response_handler(ctrl_sock, 331, None)
+  request_handler(ctrl_sock, f'PASS {sess.password}\r\n', None)
+  response_handler(ctrl_sock, 230, None)
+  request_handler(ctrl_sock, f'PASV\r\n', None)
+  data_port = response_handler(ctrl_sock, 227, None)
 
   # Data Process
   data_sock = socket.socket()
@@ -127,47 +92,42 @@ def handle_session(sess):
     data_sock.connect((sess.hostname, data_port))
   except:
     myexit(1)
-  
-  ctrl_sock.send(f'SIZE {sess.file}\r\n'.encode())
-  response = ctrl_sock.recv(255).decode()
-  fsize = int(re.findall(r'\d+', response)[-1])
 
-  # Transfer binary files
-  request = f'RETR {sess.file}\r\n'
-  ctrl_sock.send(request.encode())
-  log_list.append(f'C->S: {request}')
+  # RETR
+  request_handler(ctrl_sock, f'RETR {sess.file}\r\n', None)
+  response_handler(ctrl_sock, 150, None)
 
-  response = ctrl_sock.recv(255).decode()
-  log_list.append(f'S->C: {response}')
-  binf_code = int(response[:3])
-  check_code(binf_code, 150)
-
-  try:
-    with open(sess.file, 'wb') as fout:
-      while True:
-        data = data_sock.recv(2048)
-        if len(data) < 1:
-          break
-        fout.write(data)
-  except:
-    myexit(7)
+  # Download
+  data = bytes()
+  while True:
+    chunk = data_sock.recv(2048)
+    data += chunk
+    if len(chunk) < 1:
+      break
 
   ctrl_sock.close()
-  
-  return log_list
+
+  return data
 
 def main():
+  data = None
   args = parse_args()
-  if args.file and args.hostname:
-    sess = Session(file=args.file, hostname=args.hostname, \
-      port=args.port, username=args.username, password=args.password)
-
-    log_list = handle_session(sess)
-
-    if args.log:
-      print_log(args.log, log_list)
-
+  if args.thread and (args.file or args.hostname):
     myexit(0)
-  
+
+  if args.thread:
+    lines = open(args.thread, 'r').readlines()
+    print(lines)
+
+  elif args.file and args.hostname:
+    sess = Session(args.file, args.hostname, args.port, \
+      args.username, args.password)
+    data = session_handler(sess, args.log)
+    with open(sess.file, 'wb') as fout:
+      fout.write(data)
+    myexit(0)
+
+  return
+
 if __name__ == '__main__':
   main()
